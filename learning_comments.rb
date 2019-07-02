417f7a5c883cc8c2fb2ea0d03040365f91138234
Isaac Romano <isaac93romano@gmail.com>
2019-07-02 21:51:34 +0000

    Automatically backed up by Learn

--- learning_comments.rb
+++ learning_comments.rb
@@ -1,5 +1,3 @@
-# From the Three Dog Night song: "Joy to the world (Jeremiah was a Bullfrog)"
-puts "Joy to the world"
-puts "All the boys and girls"
-puts "Joy to the fishes in the deep blue sea"
-puts "Joy to you and me"
+name = "Isaac"
+
+puts "We're sorry, but per health inspector's rules, #{name} is not allowed in the store."
